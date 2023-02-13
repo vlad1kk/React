a312560b48bbbe86da36b6349e791a2d36986bdc
Vlad Tipukhov <vlad1kk728@ukr.net>
2023-02-13 09:56:39 +0200

    create counter

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -6,15 +6,27 @@ export default class Ccomponent extends Component {
       super(props)
     
       this.state = {
-         visibility:false,
+         count:0
       }
       // Ця прив'язка необхідна, щоб `this` працював у функції зворотнього виклику
-      this.handleClick = this.handleClick.bind(this);
+      this.increment = this.increment.bind(this);
+      this.decrement = this.decrement.bind(this);
+      this.reset = this.reset.bind(this);
     }
     //   Метод setState() призначає зміни об’єкта стану (state). У відповідь на зміни стану компонент рендериться повторно.
-    handleClick(){
+        increment(){
         this.setState(state => ({
-            visibility: !state.visibility
+            count: state.count + 1
+        }));
+      }
+        decrement(){
+        this.setState(state => ({
+            count: state.count - 1
+        }));
+      }
+        reset(){
+        this.setState(state => ({
+            count: 0
         }));
       }
     
@@ -23,24 +35,13 @@ export default class Ccomponent extends Component {
     // 2 спосіб доступу до state:
     // const name = this.state.name;
     // якщо коритсуватись цим способом то в фігурні дужки просто передається імʼя змінної (Class component {name})
-    if(this.state.visibility){
         return (
             <div>
-              <h1>Class component {this.state.name}</h1>
-              <button onClick={this.handleClick}>Click</button>
-                      {/* <Menu/> */}
-                    {/* <h1>Class component {this.props.number}</h1> */}
-                    {/* Tакож можна передавати масив:
-                    <h1>Class component {this.props.numbers.join(',')}</h1> */}
+                <button onClick={this.increment}>increment</button>
+                <button onClick={this.decrement}>decrement</button>
+                <button onClick={this.reset}>reset</button>
+                <h1>Current: {this.state.count}</h1>
             </div>
           );
-    } else {
-        return (
-            <div>
-              <button onClick={this.handleClick}>Click</button>
-            </div>
-          );
-    }
-    
   }
 }
